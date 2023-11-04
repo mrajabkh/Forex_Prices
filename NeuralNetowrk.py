ce394c339ea4417d937f72c92f93d54c1a418442
Zack Turner <zackdavidturner@gmail.com>
2023-11-04 21:02:14 +0000

    a start

--- NeuralNetowrk.py
+++ NeuralNetowrk.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
 
+files = pd.read_csv(
+    './SingleDay.csv',
+    names=['Date', 'Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume'],
+    converters={'Open': float, 'High': float, 'Low': float, 'Close': float, 'Volume': float}
+)
+dataset = files.copy()
+print(dataset['Open'].sum())
