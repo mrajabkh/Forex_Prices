f3a524a902b10bbda9839230d0bf84c764d55fe4
oxbowtarsier <oliver.fox@durham.ac.uk>
2023-11-05 04:39:33 +0000

    Made a gap in the excel spreasheet so that there can be two seperate files for year 3 and year 4

--- plot.py
+++ plot.py
@@ -9,6 +9,6 @@ with open('Year 3 and Year 4.csv',mode='r') as csv_file:
 '''
 
 df = pd.read_csv("Year 3 and Year 4.csv")
-df = df[["Date","Close"]]
+df = df[["Yr3Date","Yr3Close","Yr4Date","Yr4Close"]]
 
 
