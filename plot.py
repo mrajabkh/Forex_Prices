d01868c4b70983a93b73947d19a5279156d7106b
oxbowtarsier <oliver.fox@durham.ac.uk>
2023-11-05 04:24:29 +0000

    put the columns together on the spreadsheet, added bit of code

--- plot.py
+++ plot.py
@@ -1,1 +1,14 @@
-print ("Go and fuck yourself")
+import matplotlib.pyplot as pit
+import pandas as pd
+import numpy as np
+import csv
+
+'''
+with open('Year 3 and Year 4.csv',mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+'''
+
+df = pd.read_csv("Year 3 and Year 4.csv")
+df = df[["Date","Close"]]
+
+
