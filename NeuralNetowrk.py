4bb5143df8eace483677a2d4cf0b9c7749019d80
Rajab Ahsen <rajab.ahsen@gmail.com>
2023-11-05 00:47:02 +0000

    Replaced day functions with a week function, improving efficiency

--- NeuralNetowrk.py
+++ NeuralNetowrk.py
@@ -2,11 +2,7 @@ import pandas as pd
 import numpy as np
 import random
 
-global count
-count = 23
-a = 23
-b = 3
-end = False
+"""end = False
 
 file_object = open("data.txt","w")
 file_object.close();
@@ -17,7 +13,29 @@ while end == False:
 
 df = pd.read_csv("SingleDay.csv")
 df = df[["Date", "Close"]]
-df = df[23:]
+df = df[23:]"""
+
+
+def week():
+    df = pd.read_csv("SingleDay.csv")
+    df = df[["Date", "Close"]]
+    count = 23
+    ddf = df[count:count + 1]
+    print(ddf.to_string())
+    while count < 361:
+        for i in range(0,6):
+            if i < 4:
+                count += 24
+            elif i == 4:
+                count += 21
+            elif i == 5:
+                count += 3
+            ddf = df[count:count + 1]
+            print(ddf.to_string())
+
+week()
+
+
 
 def days(filename):
     df = pd.read_csv("SingleDay.csv")
